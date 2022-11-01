cfe32cae703ca339b08683da952b2e14a81f9dd0
AlloTheOne <Allo.busy@gmail.com>
2022-11-01 09:17:48 +0300

    Initial Commit

---
+++ testGit/testGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGitApp.swift
+//  testGit
+//
+//  Created by Alaa Alabdullah on 01/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct testGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
